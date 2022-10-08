de29e81da5f8aaf6b08f492f3d127990c6a366da
Nathan Glenn <nathan-gary.glenn@trivago.com>
2022-10-09 01:08:57 +0200

    First outline of production structure
    I'd like the test file to specify the character ranges for each node as
    well, but this doesn't seem possible with tree-sitter's testing format
    :(.

--- grammar.js
+++ grammar.js
@@ -2,7 +2,51 @@ module.exports = grammar({
   name: 'SOAR',
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => 'hello'
+    source_file: $ => repeat($._command),
+
+    _command: $ => choice(
+      $.production
+      // TODO: other kinds of commands
+    ),
+
+    production: $ => seq(
+      'sp',
+      "{",
+      $.beginning,
+      repeat1($.condition),
+      "-->",
+      repeat($.action),
+      "}"
+    ),
+
+    beginning: $ => seq(
+      $.prod_name,
+      optional($.documentation),
+      repeat($.flag)
+    ),
+
+    prod_name: $ => /[\dA-Za-z][\dA-Za-z$%&*=><?_/@:-]*/,
+    documentation: $ => seq('"', /(?:\\["]|[^"])*/, '"'),
+    flag: $ => seq(':', choice('o-support', 'i-support', 'chunk', 'default', 'interrupt', 'template')),
+
+    // TODO: "(" <commit> condType(?) idTest(?) attrValueTests(s?) ")"
+    condition: $ => seq('(', ')'),
+    action: $ => 'TODO',
+
+
+    // TODO: use /<(?=\s)/ to ensure we don't match the beginning of a variable (lookaround illegal in tree-sitter)
+    relation: $ => choice("<=>", "<>", "<=", ">=", ">", "<", "="),
+
+    // TODO: original ends with (?<!>)> but lookaround is illegal in tree-sitter
+    variable: $ => /<[A-Za-z0-9$%&*+/:=?_<>-]+>/,
+
+    //TODO: note that in Soar, || is ignored and treated like<any>.
+    quoted: $ => /\|(?:\\[|]|[^|])*\|/,
+
+    intConstant: $ => /-?[0-9]+/,
+
+    floatConstant: $ => choice($.normalFloat, $.scientificFloat),
+    normalFloat: $ => /[-+]?[0-9]*\.[0-9]*[dDfF]?/,
+    scientificFloat: $ => /[+-]?[0-9]\.[0-9]+[eE][-+]?[0-9]+[dDfF]?/
   }
 });
