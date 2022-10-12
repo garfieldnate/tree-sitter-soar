683e3b94ab9b8189f2999894a5cd5ff53c7a377a
Nathan Glenn <nathan-gary.glenn@trivago.com>
2022-10-12 16:28:12 +0200

    Port 06-conditions.t
    Make the new tests pass by porting over the remaining LHS grammar rules.
    
    We've matched the behavior of the Perl parser, which matches the
    behavior of the CSoar parser. This means that some structures parse
    but later crash Soar; I don't honestly think this should count, and will
    be revising the tests to prevent Soar-crashing structures from being
    parseable.

--- grammar.js
+++ grammar.js
@@ -22,7 +22,7 @@ module.exports = grammar({
       'sp',
       "{",
       $.beginning,
-      repeat1($.condition),
+      $.LHS,
       "-->",
       repeat($.action),
       "}"
@@ -38,10 +38,41 @@ module.exports = grammar({
     documentation: $ => seq('"', /(?:\\["]|[^"])*/, '"'),
     flag: $ => seq(':', choice('o-support', 'i-support', 'chunk', 'default', 'interrupt', 'template')),
 
-    // TODO: "(" <commit> condType(?) idTest(?) attrValueTests(s?) ")"
-    condition: $ => seq('(', ')'),
-    action: $ => 'TODO',
+    // TODO: make sure what we have parses
+    // THEN MAYBE: change grammar to match manual instead of Perl version; probably better to not parse stuff that crashes Soar, etc.
+    // TODO: what to do about the note about id_test?
+    LHS: $ => repeat1($.cond),
 
+    action: $ => /\([^)]+\)/,
+
+    cond: $ => choice($.positiveCond, $.negativeCond),
+    negativeCond: $ => seq('-', $.positiveCond),
+    positiveCond: $ => choice($.condsForOneId, $.conjunctiveCond),
+    conjunctiveCond: $ => seq('{', repeat1($.cond), '}'),
+    condsForOneId: $ => seq('(', optional($.condType), optional($.idTest), repeat($.attrValueTests), ')'),
+    condType: $ => choice('state', 'impasse'),
+
+    idTest: $ => $.test,
+    test: $ => choice($.conjunctiveTest, $.simpleTest),
+
+    attrValueTests: $ => seq(optional('-'), $.attrTest, repeat($.valueTest)),
+
+    attrTest: $ => seq('^', $.test, repeat(seq('.', $.test))),
+
+    valueTest: $ => prec.left(choice(seq(optional('+'),$.test), seq($.condsForOneId, optional('+')))),
+
+    conjunctiveTest: $ => seq('{', repeat1($.simpleTest), '}'),
+
+    simpleTest: $ => choice($.disjunctionTest, $.relationalTest, $.singleTest),
+
+    // TODO: first token should be followed by whitespace (/<<(?=\s)/, but lookahead not allowed in tree-sitter);
+    // don't have to worry about looking for whitespace after >>; if no space is there, the parser will think it's a string and fail.
+    disjunctionTest: $ => seq('<<', repeat1($.constant), '>>'),
+
+    // note that we made relation non-optional and added singleTest to simpleTest instead (seems clearer than the definition in the manual)
+    relationalTest: $ => seq($.relation, $.singleTest),
+
+    singleTest: $ => choice($.variable, $.constant),
 
     // TODO: use /<(?=\s)/ to ensure we don't match the beginning of a variable (lookaround illegal in tree-sitter)
     relation: $ => choice("<=>", "<>", "<=", ">=", ">", "<", "="),
@@ -52,10 +83,16 @@ module.exports = grammar({
     //TODO: note that in Soar, || is ignored and treated like<any>.
     quoted: $ => /\|(?:\\[|]|[^|])*\|/,
 
+    constant: $ => choice($.symConstant, $.intConstant, $.floatConstant),
+    symConstant: $ => choice($.string, $.quoted),
     intConstant: $ => /-?[0-9]+/,
-
     floatConstant: $ => choice($.normalFloat, $.scientificFloat),
+
     normalFloat: $ => /[-+]?[0-9]*\.[0-9]*[dDfF]?/,
-    scientificFloat: $ => /[+-]?[0-9]\.[0-9]+[eE][-+]?[0-9]+[dDfF]?/
-  }
+    scientificFloat: $ => /[+-]?[0-9]\.[0-9]+[eE][-+]?[0-9]+[dDfF]?/,
+
+    // TODO: do we need to port any of the rejection rules to fix ambiguous matches?
+    string: $ => /[A-Za-z0-9$%&*+\/:=?_><-]+/,
+  },
+  // conflicts: $ => [[$.state_impasse_cond, $.valueTest]]
 });
